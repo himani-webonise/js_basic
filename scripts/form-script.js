d5b15d9d98f7881a5e71eb5ccdfe7651acecd2de
himani <himani.sharma@weboniselab.com>
2017-03-24 14:58:30 +0530

    query params using expression function

--- scripts/form-script.js
+++ scripts/form-script.js
@@ -1,6 +1,15 @@
 
-var urlParams = getUrlParamString();
+var getQueryParamString = function(key) {
+	var queryParams = new URLSearchParams(document.location.search.substring(1));
+	return queryParams.get(key);
+}
 
+var user = {
+	firstName : getQueryParamString("firstname"),
+	lastName : getQueryParamString("lastname")
+}
+
+var urlParams = getUrlParamString();
 var firstName = urlParams.get("firstname");
 var lastName = urlParams.get("lastname");
 
