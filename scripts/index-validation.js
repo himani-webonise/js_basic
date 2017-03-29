26a0e7deeb8694b93f3961ac9b94f8146c244931
himani <himani.sharma@weboniselab.com>
2017-03-29 18:11:21 +0530

    added validation for gender

--- scripts/index-validation.js
+++ scripts/index-validation.js
@@ -1,72 +1,43 @@
 
-// firstName, lastName min 3 max 15
-
-// var firstName = document.getElementsByName('firstname')
-// var lastName = document.getElementsByName('lastname')
-
-// let minName = 3;
-// let maxName = 15;
-
-
-
-/*var submit = document.querySelectorAll('input[type=submit]');
-submit.addEventListener("click", nameLengthValidator(lastName, minName, maxName));
-
-function nameLengthValidator(value, min, max) {
-	var valueLength = value.length
-	console.log(valueLength)
-	if (valueLength <= min && valueLength >= max) {
-		return //
-	} else {
-		var error = value + "is not valid";
-		console.log(error);
-		return false;		
-	}
-}*/
-// var form = document.querySelectorAll('form');
-// form.addEventListener("click", test);//nameLengthValidator(lastName, minName, maxName));
-// var btn = document.getElementById('submit1');
-// alert(btn)
-// btn.addEventListener("click", function(){ alert("Hello World!"); });
-// function test() {
-// 	alert('hello');
-// }
-
 function nameLengthValidator() {
 
 	var firstName = document.getElementsByName('firstname');
 	var lastName = document.getElementsByName('lastname');
-	var gender = document.getElementById('gender');
-	
+		
 	let minName = 3;
 	let maxName = 15;
+			 
+	debugger;
 
-	if (validate(firstName, minName, maxName) && validate(lastName, minName, maxName)) {
-		console.log("true");
+	if (validate(firstName, minName, maxName) && validate(lastName, minName, maxName) && validateGender()) {		
+		var url = "file:///home/webonise/projects/app-2017/js_basic/html/succes.html"
+		reloadCurrentPage(url);
 		return true;
-	} else {
+	} else {		
+		var errorMsg = "Enter valid input";	
 		
-		var errorMsg = "Enter valid firstname and lastname";
-		alert(errorMsg);
-		var error = document.getElementById('error');
-		error.innerHTML = errorMsg;
-		reloadCurrentPage();		
+		document.getElementById('error').innerHTML = errorMsg;
+
+		return true;		
 	}	
 }
 
 function validate(value, min, max) {
-	var valueLength = value.length
-	console.log(valueLength)
-	if (valueLength <= min && valueLength >= max) {
+	var valueLength = value[0].value.length
+	if (valueLength >= min && valueLength <= max) {
 		return true;
 	}
 	return false;
 }
 
-function reloadCurrentPage() {
-	var currentURL = window.location.href;
-	window.location = currentURL;
+function validateGender() {
+	var checkBox = document.querySelectorAll('input[name=gender]:checked');
+	return !(checkBox.length == 2 || checkBox.length == 0);
+}
+
+function reloadCurrentPage(url) {	
+	window.location = url;
 }
 
 var submit = document.getElementById('submit1');
-submit1.addEventListener("click", nameLengthValidator, false);
+submit1.addEventListener("click", nameLengthValidator);
